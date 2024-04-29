52747ac7bb1e99b2ace4a07ff7855a09d4c95d2d
Gustavo Fonseca Pitombeira <gustavofonsecap@hotmail.com>
2024-04-29 11:01:16 -0300

    teste

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {},
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: '.',
+                    src: ['main.css'],
+                    dest: 'dist/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                compress: true,
+                mangle: true
+            },
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['*.js', '!*.min.js'],
+                    dest: 'dist/',
+                    ext: '.min.js'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    grunt.registerTask('build', ['less', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['build']);
+};
